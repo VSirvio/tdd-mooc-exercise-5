e2004d2a401ff7c8c2ac5cc546a79d43e0b7107d
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-05 18:31:47 +0300

    Delete all but the latest E2E test

--- e2e/tests/todo-app.spec.js
+++ e2e/tests/todo-app.spec.js
@@ -1,12 +1,6 @@
 import { test, describe, expect } from '@playwright/test';
 
 describe('Todo App', () => {
-  test('contains heading "Todos"', async ({ page }) => {
-    await page.goto('http://localhost:5173');
-
-    await expect(page.getByRole('heading', { name: 'Todos' })).toBeVisible();
-  });
-
   test('contains the message from the backend', async ({ page }) => {
     await page.goto('http://localhost:5173');
 
