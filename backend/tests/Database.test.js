d27a4f927888fec2a13c31b620b476caa0b4fed8
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-06 09:40:06 +0300

    feat: database returns an empty array in the beginning (#2)

--- backend/tests/Database.test.js
+++ backend/tests/Database.test.js
@@ -3,5 +3,6 @@ import { TodoRepository } from '../src/TodoRepository.js';
 
 describe('Database', () => {
   test('returns an empty array of todos when nothing has been added yet', async () => {
+    const todoRepository = new TodoRepository();
   });
 });
