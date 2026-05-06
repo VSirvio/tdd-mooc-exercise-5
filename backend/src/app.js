bfd1645fde91fe869e087952abbb51770c211fcf
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-06 15:42:02 +0300

    refactor: move express app to app.js

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const DATABASE_URI = process.env.DATABASE_URI;
+
+const mongoClient = new MongoClient(DATABASE_URI);
+const database = mongoClient.db('todo-app');
+const todos = database.collection('todos');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  await todos.insertOne({ content: 'Hello from database' });
+  const fetchedTodo = await todos.findOneAndDelete({});
+  res.send(fetchedTodo.content);
+});
+
+export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,6 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
+import app from './app.js';
 
-const DATABASE_URI = process.env.DATABASE_URI;
 const PORT = process.env.PORT || 3000;
-
-const mongoClient = new MongoClient(DATABASE_URI);
-const database = mongoClient.db('todo-app');
-const todos = database.collection('todos');
-
-const app = express();
-
-app.get('/', async (req, res) => {
-  await todos.insertOne({ content: 'Hello from database' });
-  const fetchedTodo = await todos.findOneAndDelete({});
-  res.send(fetchedTodo.content);
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
