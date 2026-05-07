c8de87445e8f54adf22659d7f3f13c501ea57044
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-07 20:25:14 +0300

    refactor: modify the frontend to use the proper todos API (#14)

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,6 @@ import express from 'express';
 import { MongoClient } from 'mongodb';
 import { createTodosRouter } from './controllers/todos.js';
 
-const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27018/test';
-
-const mongoClient = new MongoClient(DATABASE_URI);
-const database = mongoClient.db('todo-app');
-const todos = database.collection('todos');
-
 const createApp = todoRepository => {
   const app = express();
 
