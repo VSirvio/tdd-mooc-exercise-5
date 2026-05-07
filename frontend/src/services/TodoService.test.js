2ac161863ae102d8b3028183c6cf43bedf751133
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-07 21:14:33 +0300

    feat: todo service can fetch all todos (#1)

---
+++ frontend/src/services/TodoService.test.js
@@ -0,0 +1,9 @@
+import { describe, test } from 'vitest';
+import { TodoService } from './TodoService.js';
+
+const todoService = new TodoService();
+
+describe('Todo service', () => {
+  test('can fetch all todos', () => {
+  });
+});
