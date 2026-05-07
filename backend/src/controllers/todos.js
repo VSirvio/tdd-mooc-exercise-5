803caf13bd3895400146a031aba096a571346f97
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-07 19:42:07 +0300

    refactor: move todo controllers to a separate file (#11)

--- backend/src/controllers/todos.js
+++ backend/src/controllers/todos.js
@@ -3,5 +3,10 @@ import express from 'express';
 export const createTodosRouter = todoRepository => {
   const todosRouter = express.Router();
 
+  todosRouter.post('/', async (req, res) => {
+    const createdTodo = await todoRepository.create(req.body);
+    res.status(201).json(createdTodo);
+  });
+
   return todosRouter;
 };
