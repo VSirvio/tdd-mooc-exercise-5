fc3c8a2de4c070d6c8035f4673835fc2c1df14dd
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-07 15:54:45 +0300

    refactor: create app with createApp() function (#9)

--- backend/tests/App.test.js
+++ backend/tests/App.test.js
@@ -2,7 +2,7 @@ import { describe, test } from 'vitest';
 import request from 'supertest';
 import { app, createApp } from '../src/app.js';
 
-const app = app || createApp();
+const app2 = createApp();
 
 describe('App', () => {
   test('responds with status code 200 when fetching all todos', async () => {
