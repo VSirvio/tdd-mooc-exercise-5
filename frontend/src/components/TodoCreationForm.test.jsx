ed5bc869bd674d28f2c66a5041bbc4a11048f598
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-08 19:46:48 +0300

    feat: TodoCreationForm can submit new todo (#5)

--- frontend/src/components/TodoCreationForm.test.jsx
+++ frontend/src/components/TodoCreationForm.test.jsx
@@ -1,5 +1,6 @@
 import { describe, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import TodoCreationForm from './TodoCreationForm.jsx';
 
 describe('TodoCreationForm', () => {
