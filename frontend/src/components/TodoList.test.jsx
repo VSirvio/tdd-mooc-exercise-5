aecc32068ec04320373503951737805ffeca80bd
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-08 10:06:57 +0300

    feat: TodoList can display todos (#2)

--- frontend/src/components/TodoList.test.jsx
+++ frontend/src/components/TodoList.test.jsx
@@ -1,7 +1,14 @@
 import { describe, test } from 'vitest';
+import { render } from '@testing-library/react';
 import TodoList from './TodoList.jsx';
 
 describe('TodoList', () => {
   test('can display todos', () => {
+    const todos = [
+      { id: '69fd8a21f5342f5fb8a8fa82', content: 'Take out the trash' },
+      { id: '69fd8a874e0072ca38992d4a', content: 'Pay the bills' },
+    ];
+
+    render(<TodoList todos={todos} />);
   });
 });
