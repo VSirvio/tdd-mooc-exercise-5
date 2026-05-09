1fd43abe85da6e929fe50d908c00806362500f28
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-09 15:39:42 +0300

    feat: TodoEditForm can submit an edit for a todo (#1)

---
+++ frontend/src/components/TodoEditForm.test.jsx
@@ -0,0 +1,7 @@
+import { describe, test } from 'vitest';
+import TodoEditForm from './TodoEditForm.jsx';
+
+describe('TodoEditForm', () => {
+  test('can submit an edit for a todo', () => {
+  });
+});
