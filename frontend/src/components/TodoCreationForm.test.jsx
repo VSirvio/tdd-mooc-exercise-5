08bcd6fc044f9ef8895ead58852e56be4e035681
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-09 17:35:37 +0300

    refactor: move shared code to beforeEach() in TodoCreateForm.test.jsx (#4)

--- frontend/src/components/TodoCreationForm.test.jsx
+++ frontend/src/components/TodoCreationForm.test.jsx
@@ -6,6 +6,7 @@ import TodoCreationForm from './TodoCreationForm.jsx';
 describe('TodoCreationForm', () => {
   let user;
   beforeEach(() => {
+    user = userEvent.setup();
   });
 
   test('can submit new todo', async () => {
