1740cb5a3f2c5d54841c4fbfd90ea45825966ab6
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-09 16:04:36 +0300

    feat: TodoEditForm can submit an edit for a todo (#5)

--- frontend/src/components/TodoEditForm.jsx
+++ frontend/src/components/TodoEditForm.jsx
@@ -1,7 +1,19 @@
-const TodoEditForm = () => {
+import { useState } from 'react';
+
+const TodoEditForm = ({ handler }) => {
+  const [editedTodo, setEditedTodo] = useState('');
+
+  const submit = event => {
+    event.preventDefault();
+    handler(editedTodo);
+  };
+
   return (
-    <form>
-      <input />
+    <form onSubmit={submit}>
+      <input
+        value={editedTodo}
+        onChange={event => setEditedTodo(event.target.value)}
+      />
       <button>Save</button>
     </form>
   );
--- frontend/src/components/TodoEditForm.test.jsx
+++ frontend/src/components/TodoEditForm.test.jsx
@@ -1,12 +1,24 @@
-import { describe, test } from 'vitest';
+import { describe, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import TodoEditForm from './TodoEditForm.jsx';
 
 describe('TodoEditForm', () => {
-  test('can submit an edit for a todo', () => {
-    render(<TodoEditForm />);
+  test('can submit an edit for a todo', async () => {
+    const user = userEvent.setup();
+
+    const handler = vi.fn();
+
+    render(<TodoEditForm handler={handler} />);
 
     const inputField = screen.getByRole('textbox');
     const saveButton = screen.getByRole('button', { name: 'Save' });
+
+    const todoContent = 'Do homework';
+
+    await user.type(inputField, todoContent);
+    await user.click(saveButton);
+
+    expect(handler).toHaveBeenCalledExactlyOnceWith(todoContent);
   });
 });
