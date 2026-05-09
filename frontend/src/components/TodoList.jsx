8e8c5c5dce23513bcba50b2e439769b6303ede58
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-09 20:21:11 +0300

    feat: TodoList informs its parent when an "Edit" button is pressed (#1)

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,6 +1,11 @@
 const TodoList = ({ todos }) => (
   <ul>
-    {todos.map(todo => <li key={todo.id}>{todo.content}</li>)}
+    {todos.map(todo => (
+      <li key={todo.id}>
+        {todo.content}
+        <button>Edit</button>
+      </li>
+    ))}
   </ul>
 );
 
--- frontend/src/components/TodoList.test.jsx
+++ frontend/src/components/TodoList.test.jsx
@@ -17,4 +17,17 @@ describe('TodoList', () => {
     const element2 = screen.getByText(todos[1].content);
     expect(element2).toBeDefined();
   });
+
+  test('informs its parent when an "Edit" button is pressed', () => {
+    const todos = [
+      { id: '69fd8a21f5342f5fb8a8fa82', content: 'Take out the trash' },
+      { id: '69fd8a874e0072ca38992d4a', content: 'Pay the bills' },
+    ];
+
+    const editTodo = vi.fn();
+
+    render(<TodoList todos={todos} editTodo={editTodo} />);
+
+    const editButtons = screen.getAllByRole('button', { name: 'Edit' });
+  });
 });
