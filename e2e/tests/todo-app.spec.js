cbcc1aea8a159b8f05166712a4a1168f73093ef5
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-09 07:02:48 +0300

    Write an improved E2E test that uses the actual UI

--- e2e/tests/todo-app.spec.js
+++ e2e/tests/todo-app.spec.js
@@ -6,4 +6,14 @@ describe('Todo App', () => {
 
     await expect(page.getByText('To the database and back again').first()).toBeVisible();
   });
+
+  test('can create a new todo and then view it in the list', async ({ page }) => {
+    const todoContent = 'Do something';
+
+    await page.goto('http://localhost:5173');
+
+    await page.getByRole('textbox').fill(todoContent);
+    await page.getByRole('button', { name: 'Create' }).click();
+    await page.getByText(todoContent).waitFor();
+  });
 });
